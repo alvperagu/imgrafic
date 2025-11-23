dba7bb0afe9c3717bb5c12a71c3b7ff977859af3
alvperagu <alvaropa93@gmail.com>
2025-11-23 22:38:59 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace 'imgrafic' with your actual repo name if different
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/imgrafic/', // <-- Important for GitHub Pages
 });
